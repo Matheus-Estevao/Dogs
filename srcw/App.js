ec287fdba5ff44c7f97c960f1582b7a388860d25
Matheus Estevão <mev_53@hotmail.com>
2023-04-24 18:20:04 -0300

    Adiciona userContext

--- srcw/App.js
+++ srcw/App.js
@@ -5,20 +5,17 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
-import { UserStorage } from './UserContext';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <UserStorage>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
-          </Routes>
-          <Footer />
-        </UserStorage>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login/*" element={<Login />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
